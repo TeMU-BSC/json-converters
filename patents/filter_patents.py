9677b7b5066b8311b2a826569a6e6577b08cec4a
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-08-21 23:55:50 +0200

    refactor: performance improved by checking before spanish title and abstract

--- patents/filter_patents.py
+++ patents/filter_patents.py
@@ -14,13 +14,12 @@ import json
 import ntpath
 import os
 import sys
-from typing import List
 
 
 def main():
     '''Read some gz compressed files. Each line of the uncompresseed file is a JSON object representing a patent.
     Filter only the patents with title and abstract in Spanish language ('es' ISO code).
-    Convert from original patent's json schema to mesinesp's json schema, in addition of IPC codes and publication date.
+    Convert from original patent's json schema to our MongoDB's json schema for BvSalud (Mesinesp project), in addition of IPC codes and publication date.
     Write each filtered and converted JSON object in a new line (multilinejson format) to later easily import them into MongoDB.
     '''
 
@@ -29,8 +28,6 @@ def main():
     parser.add_argument('--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     args = parser.parse_args()
 
-    patents_in_spanish = list()
-
     for i, file in enumerate(args.infiles, start=1):
         print(f'Processing {file.name} ({i} of {len(args.infiles)})...')
 
@@ -38,32 +35,39 @@ def main():
         extension = os.path.splitext(file.name)[1]
 
         if extension != '.gz':
-            return 'The input files must be GZ compressed files.'
-        
-        with gzip.open(filename, 'r') as f:
+            print('The input files must be GZ compressed files.')
+            return
+
+        with gzip.open(file.name, 'r') as f:
             lines = f.readlines()
         
+        patents_in_mongo_format = list()
+
         for line in lines:
             patent = json.loads(line)
 
             ti_es = [title.get('text') for title in patent.get('title_localized') if title.get('language') == 'es']
             ab_es = [abstract.get('text') for abstract in patent.get('abstract_localized') if abstract.get('language') == 'es']
+
+            # If this patent hasn't title or abstract in Spanish, jump to the next patent. 
+            if not ti_es or not ab_es:
+                continue 
+
             ipc_codes = [ipc.get('code') for ipc in patent.get('ipc')]
 
-            if ti_es and ab_es:
-                mesinesp_format = {
-                    '_id': patent.get('publication_number'),
-                    'ti_es': html.unescape(ti_es[0]),
-                    'ab_es': html.unescape(ab_es[0]),
-                    'ipc_codes': ipc_codes,
-                    'publication_date': patent.get('publication_date'),
-                }
-                patents_in_spanish.append(mesinesp_format)
+            patents_in_mongo_format.append({
+                '_id': patent.get('publication_number'),
+                'ti_es': html.unescape(ti_es[0]),
+                'ab_es': html.unescape(ab_es[0]),
+                'ipc_codes': ipc_codes,
+                'publication_date': patent.get('publication_date'),
+            })
 
-    with open(args.outfile.name, 'w') as f:
-        for patent in patents_in_spanish:
-            json.dump(patent, f, ensure_ascii=False)
-            f.write('\n')
+        # Append filtered patents to output file once per GZ file read.
+        with open(args.outfile.name, 'a') as f:
+            for patent in patents_in_mongo_format:
+                json.dump(patent, f, ensure_ascii=False)
+                f.write('\n')
 
 
 if __name__ == '__main__':
